bcc1e7d4c46c1bc7cde900585ed93c0a77eb205e
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-16 10:25:31 -0500

    Fullstack part2 section c : axios and promises

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 
 import App from './App'
+
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log("notes:",notes)
+    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+  })
+
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+
 
 const notes = [
   {
