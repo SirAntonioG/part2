8fd96ba78f96d514596c62d2ba932470eebf25ad
AntonGreg <antonio.pena@epn.edu.ec>
2022-08-18 15:36:59 -0500

    Fullstack part2 section d : sending data to server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App'
+import App from "./App";
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    // console.log("notes:",notes)
-    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
-  })
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
